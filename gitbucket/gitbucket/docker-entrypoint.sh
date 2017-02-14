3bb8c59efed53ae0a83d4ea306a36e4cbe74596f
YoshinoriN <nayo-msoft@outlook.jp>
2017-02-14 18:09:06 +0900

    Fix docker-entrypoint.sh

--- gitbucket/gitbucket/docker-entrypoint.sh
+++ gitbucket/gitbucket/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cat <<EOF /gitbucket/database.conf >
+cat > /usr/libexec/gitbucket/database.conf <<-EOCONF
 db {
   url = "$MYSQL_URL"
   user = "$MYSQL_USER"
   password = "$MYSQL_PASSWORD"
 }
-EOF
+EOCONF
 
-exec java -jar /opt/gitbucket.war 
+exec java -jar /usr/libexec/gitbucket/gitbucket.war 
